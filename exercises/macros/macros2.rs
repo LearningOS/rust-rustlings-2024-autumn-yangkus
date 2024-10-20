9597d740db855c0a6f91374c832df9f4b3016aee
yangkus <yangkus@126.com>
2024-10-20 10:36:13 +0800

    to finishi macros

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -4,13 +4,18 @@
 // hint.
 
 // I AM NOT DONE
-
-fn main() {
-    my_macro!();
-}
+// macros2.rs
+//
+// Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
+// hint.
+// macros2.rs
 
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
+}
+
+fn main() {
+    my_macro!(); // 调用宏
 }
