db00ce270b32fb07243291c7042b1225a14f57d8
yangkus <yangkus@126.com>
2024-10-20 10:33:02 +0800

    to finishi threads

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -1,32 +1,26 @@
- // threads1.rs
-//
-// This program spawns multiple threads that each run for at least 250ms, and
-// each thread returns how much time they took to complete. The program should
-// wait until all the spawned threads have finished and should collect their
-// return values into a vector.
-//
-// Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() {
     let mut handles = vec![];
+    
+    // 创建10个线程
     for i in 0..10 {
         handles.push(thread::spawn(move || {
             let start = Instant::now();
-            thread::sleep(Duration::from_millis(250));
+            thread::sleep(Duration::from_millis(250)); // 模拟线程工作
             println!("thread {} is complete", i);
-            start.elapsed().as_millis()
+            start.elapsed().as_millis() // 返回线程运行的时间
         }));
     }
 
     let mut results: Vec<u128> = vec![];
+    
+    // 收集线程返回值
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        // 调用 join() 方法来获取每个线程的返回值
+        let result = handle.join().expect("Thread panicked"); // 处理线程中的 panic
+        results.push(result); // 将返回值存入 results 向量
     }
 
     if results.len() != 10 {
@@ -34,6 +28,8 @@ fn main() {
     }
 
     println!();
+    
+    // 输出每个线程的执行时间
     for (i, result) in results.into_iter().enumerate() {
         println!("thread {} took {}ms", i, result);
     }
--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,12 +1,6 @@
-// threads3.rs
-//
-// Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
@@ -26,26 +20,38 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
+fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> Arc<Mutex<u32>> {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    let tx1 = tx.clone();
+    let tx2 = tx;
+
+    let counter = Arc::new(Mutex::new(0u32));
+    let counter1 = Arc::clone(&counter);
+    let counter2 = Arc::clone(&counter);
 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
+        let mut num = counter1.lock().unwrap();
+        *num += 1;
     });
 
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
+        let mut num = counter2.lock().unwrap();
+        *num += 1;
     });
+
+    counter
 }
 
 fn main() {
@@ -53,14 +59,22 @@ fn main() {
     let queue = Queue::new();
     let queue_length = queue.length;
 
-    send_tx(queue, tx);
+    let counter = send_tx(queue, tx);
 
     let mut total_received: u32 = 0;
     for received in rx {
         println!("Got: {}", received);
         total_received += 1;
+        if total_received == queue_length {
+            break;
+        }
+    }
+
+    // Wait for both threads to finish
+    while *counter.lock().unwrap() < 2 {
+        thread::sleep(Duration::from_millis(100));
     }
 
     println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length)
+    assert_eq!(total_received, queue_length);
 }
