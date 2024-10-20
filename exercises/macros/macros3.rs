5bc759620eab0c367a55c91bd7f0747d22070bc2
yangkus <yangkus@126.com>
2024-10-20 22:35:46 +0800

    to finishi macros3

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,15 +5,17 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-//
 mod macros {
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+
+    // 公开宏以便在其他地方使用
+    pub(crate) use my_macro;
 }
 
 fn main() {
-    my_macro!();
+    macros::my_macro!();
 }
